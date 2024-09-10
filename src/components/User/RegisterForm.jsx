58790ca3ed34b2211bf4f6cc9ca8bb5916f9be0b
SebastianSamboni <sebas_18_08@hotmail.com>
2024-09-10 13:39:26 -0500

    feat: create register form structure

--- src/components/User/RegisterForm.jsx
+++ src/components/User/RegisterForm.jsx
@@ -1,7 +1,21 @@
 const RegisterForm = () => {
     return (
         <div>
-            Este es el register form
+            <h1>CREACIÓN DE CUENTA</h1>
+            <label htmlFor=''>Nombres</label>
+            <input type='text' />
+            <label htmlFor=''>Apellidos</label>
+            <input type='text'/>
+            <label htmlFor=''>Nombre de Usuario</label>
+            <input type='text' />
+            <label htmlFor=''>Correo electrónico</label>
+            <input type='email' />
+            <label htmlFor=''>Contraseña</label>
+            <input type='password' />
+            <label htmlFor=''>Confirmar Contraseña</label>
+            <input type='password' />
+            <button>Crear cuenta</button>
+            <button>Ya tengo una cuenta!</button>
         </div>
     )
 }
