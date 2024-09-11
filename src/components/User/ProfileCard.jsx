a79d7bcbbeaf1cfcf2d15522743a98c51c788ac5
SebastianSamboni <sebas_18_08@hotmail.com>
2024-09-11 11:59:46 -0500

    feat: add structure to ProfileCard component

--- src/components/User/ProfileCard.jsx
+++ src/components/User/ProfileCard.jsx
@@ -1,7 +1,40 @@
 const ProfileCard = () => {
     return (
         <div>
-            Información del usuario
+            <div>
+                <h1>INFORMACIÓN PERSONAL</h1>
+                <div>
+                    <img alt='profile-img' />
+                </div>
+                <div>
+                    <h3>Nombre</h3>
+                    <p>Nombre + Apellido Usuario</p>
+                    <h3>Nombre de Usuario</h3>
+                    <p>Nombre de usuario</p>
+                    <h3>Correo electrónico</h3>
+                    <p>Correo del usuario</p>
+                    <div>
+                        <p>Lvl. X</p>
+                        Barra de experiencia
+                        <p>0/xp_required</p>
+                    </div>
+                    <button>Actualizar información</button>
+                    <button>Actualizar contraseña</button>
+                </div>
+            </div>
+            <div>
+                <h2>Hobbies y Gustos</h2>
+                <div>
+                    <p>Un item por cada hobby</p>
+                </div>
+                <button>Agregar Hobby Nuevo o Botón con ícono</button>
+            </div>
+            <div>
+                Recordatorios
+            </div>
+            <div>
+                Tablas o gráficas de análisis / llenar con más información
+            </div>
         </div>
     )
 }
