911304ba7de8e14dce8069f66373de78f1819288
SebastianSamboni <sebas_18_08@hotmail.com>
2024-09-11 13:57:25 -0500

    fix: add button to open AvatarTable component feat: add functionality to open and close AvatarTable component

--- src/components/User/AvatarTable.jsx
+++ src/components/User/AvatarTable.jsx
@@ -1,9 +1,21 @@
-const AvatarTable = () => {
+import PropTypes from 'prop-types'
+
+const AvatarTable = ({ onClose }) => {
     return (
         <div>
-            Tabla de avatares
+            <div>
+                div contenedor de las imágenes
+                <div>Un div por cada imagen</div>
+            </div>
+            <button onClick={onClose}>
+                Cambiar Avatar
+            </button>
         </div>
     )
+}
+
+AvatarTable.propTypes = {
+    onClose: PropTypes.func.isRequired
 }
 
 export default AvatarTable
--- src/components/User/ProfileCard.jsx
+++ src/components/User/ProfileCard.jsx
@@ -1,9 +1,12 @@
 import { useState } from "react"
 import UpdateForm from "./UpdateForm"
+import AvatarTable from "./AvatarTable"
 
 const ProfileCard = () => {
     const [formType, setFormType] = useState(null)
     const [isModalOpen, setIsModalOpen] = useState(false)
+    const [showEditButton, setShowEditButton] = useState(false)
+    const [isAvatarTableOpen, setIsAvatarTableOpen] = useState(false)
 
     const handleFormChange = type => {
         setFormType(type)
@@ -14,12 +17,39 @@ const ProfileCard = () => {
         setIsModalOpen(false)
     }
 
+    const handleOpenAvatarTable = () => {
+        setIsAvatarTableOpen(true)
+    }
+    const closeAvatarTable = () => {
+        setIsAvatarTableOpen(false)
+    }
+
     return (
         <div>
             <h1>INFORMACIÓN PERSONAL</h1>
-            <div>
-                <img alt='profile-img' />
+            <div onMouseEnter={() => setShowEditButton(true)}
+                onMouseLeave={() => setShowEditButton(false)}
+            >
+                <img alt='profile-img' src='https://res.cloudinary.com/dgm8dtsce/image/upload/v1725937229/avatars/default-img-avatar-tskmng-18.jpg' />
+                {showEditButton && (
+                    <button onClick={handleOpenAvatarTable}
+                        style={{
+                            position: 'absolute',
+                            top: '10px',
+                            right: '10px',
+                            backgroundColor: 'white',
+                            borderRadius: '50%',
+                            border: 'none',
+                            padding: '5px',
+                            cursor: 'pointer',
+                            boxShadow: '0px 4px 8px rgba(0,0,0,0.2)'
+                        }}
+                    >✏️</button>
+                )}
             </div>
+
+            {isAvatarTableOpen && <AvatarTable onClose={closeAvatarTable} />}
+
             <div>
                 <h3>Nombre</h3>
                 <p>Nombre + Apellido Usuario</p>
