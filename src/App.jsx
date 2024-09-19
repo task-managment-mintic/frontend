092dd772a9112e96702afb9b6f0ecd77eb4563db
SebastianSamboni <sebas_18_08@hotmail.com>
2024-09-19 15:45:59 -0500

    feat: implement protection on routes if user is not authenticated feat: implement access denied to login/register page if user is logged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Init from "./pages/Init"
-import Home from "./pages/Home"
-import Profile from "./pages/Profile"
-import { AuthProvider } from "./context/AuthContext"
-import NavBar from "./components/NavBar"
-import { HobbyProvider } from "./context/HobbyContext"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Init from './pages/Init'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import { AuthProvider } from './context/AuthContext'
+import NavBar from './components/NavBar'
+import { HobbyProvider } from './context/HobbyContext'
+import ProtectedRoute from './ProtectedRoute'
 
 const App = () => {
     return (
@@ -15,8 +16,10 @@ const App = () => {
                         <NavBar />
                         <Routes>
                             <Route path='/' element={<Init />} />
-                            <Route path='/home' element={<Home />} />
-                            <Route path='/profile' element={<Profile />} />
+                            <Route element={<ProtectedRoute />}> 
+                                <Route path='/home' element={<Home />} />
+                                <Route path='/profile' element={<Profile />} />
+                            </Route>
                         </Routes>
                     </BrowserRouter>
                 </div>
