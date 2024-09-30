f648b79e5f9d59dc47736b6e0a6fd86e3be10a5b
SebastianSamboni <sebas_18_08@hotmail.com>
2024-09-30 10:40:46 -0500

    fix: change NavBar into a SideBar with options stylized and logout button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
+import { Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, Toolbar, Typography } from '@mui/material'
+import { Home, ManageAccounts } from '@mui/icons-material'
 
 const NavBar = () => {
     const { signOut } = useAuth()
@@ -11,17 +13,58 @@ const NavBar = () => {
     }
 
     return (
-        <div>
-            <ul>
-                <li>
+        <Drawer variant='permanent'
+            anchor='left'
+            sx={{
+                width: '200px',
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                    width: '200px',
+                    boxSizing: 'border-box',
+                    bgcolor: '#000',
+                    color: '#fff'
+                }
+            }}
+        >
+            <Toolbar>
+                <Typography>
+                    Logo APP
+                </Typography>
+            </Toolbar>
+            <Divider />
+            <List sx={{ marginTop: '100px' }}>
+                <ListItem>
+                    <ListItemIcon>
+                        <Home sx={{ color: '#fff' }} />
+                    </ListItemIcon>
                     <Link to='/home'>Inicio</Link>
-                </li>
-                <li>
+                </ListItem>
+                <ListItem>
+                    <ListItemIcon>
+                        <ManageAccounts sx={{ color: '#fff' }}/>
+                    </ListItemIcon>
                     <Link to='/profile'>Mi Perfil</Link>
-                </li>
-            </ul>
-            <button onClick={handleSignOut}>Cerrar sesión</button>
-        </div>
+                </ListItem>
+            </List>
+            <Divider />
+            <List sx={{ marginTop: '80px '}}>
+                <ListItem>
+                    <ListItemButton sx={{
+                        color: '#000',
+                        backgroundColor: '#c3bc7c',
+                        borderRadius: 2,
+                        '&:hover': {
+                            backgroundColor: '#fff',
+                            color: '#000'
+                        }
+                    }}
+                        onClick={handleSignOut}
+                    >
+                        Cerrar Sesión
+                    </ListItemButton>
+                </ListItem>
+            </List>
+        </Drawer>
     )
 }
 
